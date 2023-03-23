4e153be9c52c6c0408e0d8b61c435c220f91b096
Birappa88 <birappa.mangod@gmail.com>
2023-03-23 15:58:18 +0530

    About Dom

---
+++ JavaScript/day23.js
@@ -0,0 +1,34 @@
+// window
+
+// DOM
+// document
+
+// BOM
+// navigator
+// screen
+// location
+// frames
+// history
+// XMLHttpRequest
+
+// JavaScript
+// Array
+// Object
+// Function
+
+
+
+// function sayHi() {
+//     alert("Hello");
+// }
+
+// // global functions are methods of the global object:
+// window.sayHi();
+
+// alert(window.innerHeight); // inner window height
+
+// change the background color to red
+document.body.style.background = "red";
+
+// change it back after 1 second
+setTimeout(() => document.body.style.background = "", 1000);
