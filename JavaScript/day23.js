06c28ab158f098ccec0979315b306a83a1bc4eec
Birappa88 <birappa.mangod@gmail.com>
2023-03-24 12:36:42 +0530

    date24 till 12.30pm

--- JavaScript/day23.js
+++ JavaScript/day23.js
@@ -28,7 +28,26 @@
 // alert(window.innerHeight); // inner window height
 
 // change the background color to red
-document.body.style.background = "red";
+// document.body.style.background = "red";
 
-// change it back after 1 second
-setTimeout(() => document.body.style.background = "", 1000);
+// // change it back after 1 second
+// setTimeout(() => document.body.style.background = "", 1000);
+
+
+
+let obj = { hsd: "sdkfhl", jdfkldj: "skjdkf" }
+let obj2 = obj
+
+obj2.hsd = "sesss"
+
+let obj3 = { ...obj }
+
+// Map, filter, reduce, find, some, foreach
+
+let arr = [2, 23, 1, 3, 4]
+let arr1 = [2, 23, 1, 3, 4]
+let sum = 0
+let arrr = arr.reduce((i, k) => i += k, 0)
+console.log(arrr)
+
+// nullish 
